d550eb0782c8fc147732fbccc25b7f0e1e9fad15
xaraf <mohamed.ce.ao@gmail.com>
2024-06-04 14:15:49 +0100

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,9 @@
 import net from "net";
 
 const server = net.createServer((socket) => {
+  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
   socket.end();
 });
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 server.listen(4221, "localhost", () => {
